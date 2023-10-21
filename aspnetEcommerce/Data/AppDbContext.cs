096795ad6aaa55a61e672dc64e550d11da9160c2
uzay.durdu <uzay.durdu@uni-konstanz.de>
2023-10-21 14:32:20 +0200

    db-created

--- aspnetEcommerce/Data/AppDbContext.cs
+++ aspnetEcommerce/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using aspnetEcommerce.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace aspnetEcommerce.Data
 {
@@ -8,5 +9,7 @@ namespace aspnetEcommerce.Data
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
